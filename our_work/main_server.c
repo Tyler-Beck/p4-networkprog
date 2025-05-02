914e1e0c61d31c6ca5c5e7afd955d2d525d196fc
Tyler <tybeqqfn@gmail.com>
2025-05-02 19:48:46 -0400

    Progress to main_server

--- our_work/main_server.c
+++ our_work/main_server.c
@@ -5,18 +5,49 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
 #define PORT_NUM 1004
 
-
-void display_clients();
 void add_client();
 void remove_client();
+void display_clients();
 void broadcast_msg();
 void notification();
 void assign_color();
 
+/* 
+	Everything so far is basically from chat_server_full in /ex folder.
+	I think the functions above are all we need for checkpoint 1.
+*/
 
+typedef struct client_info{
+	int sockfd;
+	char username[20];
+	char ip_addr[20];
+	struct client_info* next; // pointer to the next client
+} Client;
+
+Client* head = NULL; // head of the linked list
+Client* tail = NULL; // tail of the linked list
+int num_clients = 0; // connected clients
+
+
+void add_client(int newclisockfd, char* username, char* ip_addr){
+	// Add a new client
+	if(head == NULL){
+		head = (Client*) malloc(sizeof(Client));
+		head->sockfd = newclisockfd;
+		head->next = NULL;
+		tail = head;
+	} else {
+		tail->next = (Client*) malloc(sizeof(Client));
+		tail->next->sockfd = newclisockfd;
+		tail->next->next = NULL;
+		tail = tail->next;
+	}
+}
 
 void error(const char *msg){
 	perror(msg);
@@ -24,6 +55,8 @@ void error(const char *msg){
 }
 
 int main(int argc, char* argv[]){
+
+	if(argc < 2) error("Specify port number");
 
 
     return 0;
